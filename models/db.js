f0b1134da5da549ce5d66c97fbc93fb8ff501fa1
Bishal7735 <bishalupto@gmail.com>
2026-04-01 18:11:45 +0530

    feat: initialize Sequelize database connection and integrate into app configuration

--- models/db.js
+++ models/db.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('task_manager', 'root', 'root', {
+const sequelize = new Sequelize('task_manager', 'root', 'ROOT', {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
